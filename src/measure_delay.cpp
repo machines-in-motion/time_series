0502d43a6a10648fc5dba82612c768e30ab19e8e
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-01-22 11:35:40 +0100

    Extend to multi-process

--- src/measure_delay.cpp
+++ src/measure_delay.cpp
@@ -2,12 +2,28 @@
  * @file
  * @brief Small application to compare time delay of single- and multi-process
  *        time series.
+ *
+ * One thread/process writes the current time stamp to the time series at a
+ * fixed rate.  The other one reads it, computes the elapsed time and prints
+ * some simple analysis on this when finished.
+ *
+ * Expects a "mode" argument which has to be one of the following values:
+ *
+ * - "single": Using single-process time series and running sender and receiver
+ *   in separate real-time threads.
+ * - "multi_write": Using multi-process time series and running only the sender
+ *   which writes to the time series (using a real-time thread).
+ * - "multi_read": Using multi-process time series and running only the receiver
+ *   which reads from the time series (using a real-time thread).  Also performs
+ *   the analysis in the end.
+ *
  * @copyright Copyright (c) 2020, Max Planck Gesellschaft.
  */
 
 #include <Eigen/Eigen>
 #include <limits>
 #include <real_time_tools/thread.hpp>
+#include <time_series/multiprocess_time_series.hpp>
 #include <time_series/time_series.hpp>
 
 const unsigned int NUM_STEPS = 10000;
@@ -21,14 +37,14 @@ void *send(void *args)
     // wait a bit to ensure the other loop is waiting
     real_time_tools::Timer::sleep_sec(1);
 
-    time_series::TimeSeries<double> &ts =
-        *static_cast<time_series::TimeSeries<double> *>(args);
+    time_series::TimeSeriesInterface<double> &ts =
+        *static_cast<time_series::TimeSeriesInterface<double> *>(args);
     std::cout << "start transmitting" << std::endl;
     for (unsigned int i = 0; i < NUM_STEPS; i++)
     {
-        double now = real_time_tools::Timer::get_current_time_sec();
-        ts.append(now);
-        real_time_tools::Timer::sleep_until_sec(now + 0.001);
+        ts.append(real_time_tools::Timer::get_current_time_sec());
+        // TODO verify that the sleep here is long enough
+        real_time_tools::Timer::sleep_sec(0.001);
     }
     // indicate end by sending a NaN
     ts.append(std::numeric_limits<double>::quiet_NaN());
@@ -37,8 +53,8 @@ void *send(void *args)
 
 void *receive(void *args)
 {
-    time_series::TimeSeries<double> &ts =
-        *static_cast<time_series::TimeSeries<double> *>(args);
+    time_series::TimeSeriesInterface<double> &ts =
+        *static_cast<time_series::TimeSeriesInterface<double> *>(args);
 
     size_t t = 0;
     std::cout << "ready for receiving" << std::endl;
@@ -57,35 +73,100 @@ void *receive(void *args)
     return nullptr;
 }
 
-/**
- * @brief Read and display the values from the time series
- */
-void run()
+enum Mode
 {
-    time_series::TimeSeries<double> ts(100);
+    SINGLE,
+    MULTI_WRITE,
+    MULTI_READ
+};
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Expect on of the following values as argument: single, "
+                     "multi_write, multi_read"
+                  << std::endl;
+        return -1;
+    }
+
+    Mode mode;
+    if (strcmp(argv[1], "single") == 0)
+    {
+        std::cout << "Single-process mode." << std::endl;
+        mode = SINGLE;
+    }
+    else if (strcmp(argv[1], "multi_write") == 0)
+    {
+        std::cout << "Multi-process write mode." << std::endl;
+        mode = MULTI_WRITE;
+    }
+    else if (strcmp(argv[1], "multi_read") == 0)
+    {
+        std::cout << "Multi-process read mode." << std::endl;
+        mode = MULTI_READ;
+    }
+    else
+    {
+        std::cerr << "Invalid argument" << std::endl;
+        return -1;
+    }
 
     real_time_tools::RealTimeThread thread_send, thread_receive;
 
-    thread_receive.create_realtime_thread(&receive, &ts);
-    thread_send.create_realtime_thread(&send, &ts);
+    switch (mode)
+    {
+        case SINGLE:
+        {
+            time_series::TimeSeries<double> ts(100);
 
-    thread_receive.join();
-    thread_send.join();
+            thread_receive.create_realtime_thread(&receive, &ts);
+            thread_send.create_realtime_thread(&send, &ts);
 
-    // for (size_t i = 0; i < NUM_STEPS; i++) {
-    //    std::cout << g_delays[i] << std::endl;
-    //}
-    double std_dev = std::sqrt((g_delays - g_delays.mean()).square().sum() /
-                               (g_delays.size() - 1));
-    std::cout << "Mean delay: " << g_delays.mean() << std::endl;
-    std::cout << "Min. delay: " << g_delays.minCoeff() << std::endl;
-    std::cout << "Max. delay: " << g_delays.maxCoeff() << std::endl;
-    std::cout << "std dev:    " << std_dev << std::endl;
-}
+            thread_receive.join();
+            thread_send.join();
+        }
+        break;
 
-int main()
-{
-    run();
+        case MULTI_WRITE:
+        {
+            time_series::MultiprocessTimeSeries<double> ts(
+                "measure_delay", 100, false);
+
+            thread_send.create_realtime_thread(&send, &ts);
+
+            thread_send.join();
+        }
+        break;
+
+        case MULTI_READ:
+        {
+            time_series::clear_memory("measure_delay");
+            time_series::MultiprocessTimeSeries<double> ts(
+                "measure_delay", 100, true);
+
+            thread_receive.create_realtime_thread(&receive, &ts);
+
+            thread_receive.join();
+        }
+        break;
+
+        default:
+            // do nothing
+            break;
+    }
+
+    if (mode == SINGLE || mode == MULTI_READ)
+    {
+        // analyse measured delays
+        double std_dev = std::sqrt((g_delays - g_delays.mean()).square().sum() /
+                                   (g_delays.size() - 1));
+        std::cout << "Mean delay: " << g_delays.mean() << std::endl;
+        std::cout << "Min. delay: " << g_delays.minCoeff() << std::endl;
+        std::cout << "Max. delay: " << g_delays.maxCoeff() << std::endl;
+        std::cout << "std dev:    " << std_dev << std::endl;
+    }
+
     return 0;
 }
 
